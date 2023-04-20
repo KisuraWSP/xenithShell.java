bf2417ad65896259cfd0eb033f109bb7126156f9
Kisura W.S.P <85266425+KisuraWSP@users.noreply.github.com>
2023-04-20 22:34:05 +0530

    Update module-info.java

--- XenithShell/src/module-info.java
+++ XenithShell/src/module-info.java
@@ -6,13 +6,7 @@
  *
  */
 module Shell{
-	/*To do 
-	 * improve custom exceptions and add more
-	 * improve the code base and make code readable and improved	 
-	 * fix all the functions code
-	 * make it readable
-	 * make sure the comments follow jsdoc 3
-	 * make sure that when program is run, it doesn't take too much compilation time
-	 * change all the variable names and syntaxes so that it is meaningful 
-	 */
+	/*
+  *Shell Program
+  */
 }
